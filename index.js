e59caa632834818792cbfab003b8eef0515d09a4
Madson Santos <madsonsamsung@gmail.com>
2025-04-18 14:33:53 -0300

    implements send text and midia messages

--- index.js
+++ index.js
@@ -25,4 +25,47 @@ async function sendTemplateMessage(req, res) {
     console.log(response.data);
 }
 
-sendTemplateMessage();
+async function sendTextMessage(req, res) {
+    const response = await axios({
+        url: `https://graph.facebook.com/v14.0/${process.env.WHATSAPP_PHONE_ID}/messages`,
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${process.env.WHATSAPP_TOKEN}`,
+            'Content-Type': 'application/json'
+        },
+        data: JSON.stringify({
+            messaging_product: 'whatsapp',
+            to: process.env.WHATSAPP_PHONE_TO,
+            type: 'text',
+            text: {
+                body: 'This is a test message from Node.js!'
+            }
+        })
+    });
+
+    console.log(response.data);
+}
+
+async function sendMediaMessage(req, res) {
+    const response = await axios({
+        url: `https://graph.facebook.com/v14.0/${process.env.WHATSAPP_PHONE_ID}/messages`,
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${process.env.WHATSAPP_TOKEN}`,
+            'Content-Type': 'application/json'
+        },
+        data: JSON.stringify({
+            messaging_product: 'whatsapp',
+            to: process.env.WHATSAPP_PHONE_TO,
+            type: 'image',
+            image: {
+                link: 'https://dummyimage.com/600x400/000/fff.png&text=Hello+World',
+                caption: 'This is a test image from Node.js!'
+            }
+        })
+    });
+
+    console.log(response.data);
+}
+
+sendMediaMessage();
